8b1a460ba9deae279a84445f320e193961787488
Alok <alokkhalkho65@gmail.com>
2024-02-07 21:03:35 +0530

    user controller added for registration

--- src/app.js
+++ src/app.js
@@ -17,6 +17,13 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public"));
 
-app.use(cookieparser())
+app.use(cookieparser());
+
+
+//routes import
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
